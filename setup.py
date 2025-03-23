5d162a504389e76fb8cb97dd1f2e72fd258b376c
Darshil Mahraur <darshilmahraur67@gmail.com>
2025-03-23 21:43:03 +0530

    added to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="FrameSnap",
-    version="1",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         "opencv-python",
@@ -13,6 +13,8 @@ setup(
         ],
     },
     author="Darshil",
+    author_email="darshilmahraur67@gmail.com", 
     description="A Python library to extract frames from videos and save them as images.",
     url="https://github.com/darshil89/FrameSnap",
+    python_requires=">=3.6",  # ✅ Minimum Python version
 )
